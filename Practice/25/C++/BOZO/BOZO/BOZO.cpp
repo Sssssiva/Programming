ecf17ec97fbc16424e9e1bf75798b951f759eece
Alexander <molfix2010@yandex.ua>
2020-12-09 22:35:23 +0300

    П\З за 09.11.20

---
+++ Practice/25/C++/BOZO/BOZO/BOZO.cpp
@@ -0,0 +1,117 @@
+﻿#include<iostream>
+#include<vector>
+#include<ctime>
+#include<cmath>
+
+std::vector<int> BozoSort(std::vector<int>a, bool check = true) {
+	bool sorted = false;
+	while (!sorted) {
+		int x1 = rand() % a.size();
+		int x2 = rand() % a.size();
+
+		int swap = a[x1];
+		a[x1] = a[x2];
+		a[x2] = swap;
+
+		sorted = true;
+		if (check) {
+			for (int i = 1; i < a.size(); i++) {
+				if (a[i - 1] > a[i]) {
+					sorted = false;
+					break;
+				}
+			}
+		}
+		else {
+			for (int i = 1; i < a.size(); i++) {
+				if (a[i - 1] < a[i]) {
+					sorted = false;
+					break;
+				}
+			}
+		}
+	}
+
+	return a;
+}
+
+std::vector<int> BozoSort(std::vector < std::vector < int>>a, bool check = true) {
+	std::vector<int>help;
+	for (int i = 0; i < a.size(); i++) {
+		for (int j = 0; j < a.size(); j++) {
+			help.push_back(a[i][j]);
+		}
+	}
+	return BozoSort(help, check);
+};
+
+std::vector<int> BozoSort(int first, int second, int third, bool check = true) {
+	std::vector<int>three;
+	three.push_back(first);
+	three.push_back(second);
+	three.push_back(third);
+	return BozoSort(three, check);
+}
+
+int main() {
+	srand(time(0));
+	std::vector <int> a;
+	std::vector <int> temp;
+	int n;
+	std::cin >> n;
+
+	int x;
+	for (int i = 0; i < n; i++) {
+		std::cin >> x;
+		a.push_back(x);
+	}
+	temp = BozoSort(a);
+	std::cout << std::endl;
+	for (int i = 0; i < temp.size(); i++) {
+		std::cout << temp[i] << " ";
+	}
+	std::cout << std::endl;
+	bool check;
+	temp = BozoSort(a, check = false);
+	for (int i = 0; i < temp.size(); i++) {
+		std::cout << temp[i] << " ";
+	}
+	
+	std::vector<std::vector<int>> a2;
+	int remember = 0;
+	for (int i = 0; i < sqrt(n); i++) {
+		std::vector<int> temp1;
+		for (int j = 0; j < sqrt(n); j++) {
+			temp1.push_back(a[remember]);
+			remember++;
+		}
+		a2.push_back(temp1);
+		if (remember > n) {
+			break;
+		}
+	}
+	temp = BozoSort(a2);
+	std::cout << std::endl;
+	for (int i = 0; i < temp.size(); i++) {
+		std::cout << temp[i] << " ";
+	}
+	temp = BozoSort(a2, check = false);
+	std::cout << std::endl;
+	for (int i = 0; i < temp.size(); i++) {
+		std::cout << temp[i] << " ";
+	}
+
+	int first = a[0], second = a[1], third = a[2];
+
+	temp = BozoSort(first, second, third);
+	std::cout << std::endl;
+	for (int i = 0; i < temp.size(); i++) {
+		std::cout << temp[i] << " ";
+	}
+
+	temp = BozoSort(first, second, third, check = false);
+	std::cout << std::endl;
+	for (int i = 0; i < temp.size(); i++) {
+		std::cout << temp[i] << " ";
+	}
+}
